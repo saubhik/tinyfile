daf99ee57f5ceff2b3dd9d9999334fd72baa8f9d
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2021-03-10 09:39:21 -0500

    Complete client.c implementation.

--- client.c
+++ client.c
@@ -12,8 +12,9 @@ int main(int argc, char **argv) {
             {0, 0,                       0, 0}
     };
 
-    tinyfile_arg_t arg, out;
     char call_method; // 'a' for async, 's' for sync
+    char file_path[150], file_list_path[150];
+    unsigned short int file_list = 0;
 
     int option_index = 0, c;
     while ((c = getopt_long(argc, argv, "f:l:s:", long_options, &option_index)) != -1) {
@@ -27,11 +28,13 @@ int main(int argc, char **argv) {
 
             case 'f':
                 printf("option -f with value %s\n", optarg);
-                memcpy(arg.source_file_path, optarg, strlen(optarg));
+                strcpy(file_path, optarg);
                 break;
 
             case 'l':
                 printf("option -l with value %s\n", optarg);
+                file_list = 1;
+                strcpy(file_list_path, optarg);
                 break;
 
             case 's':
@@ -45,42 +48,102 @@ int main(int argc, char **argv) {
                 break;
 
             default:
+                printf("Required options (--file or --files) with --state");
                 abort();
         }
     }
 
-    printf("arg.source_file_path = %s, call_method = %c\n", arg.source_file_path, call_method);
-
-
     tinyfile_init();
 
-    tinyfile_request_entry_idx_t entry_index;
     tinyfile_request_priority_t priority = 1;
 
-    if (call_method == 'a') {
-        /* Asynchronous call to library */
-        printf("Making an asynchronous call to tinyfile service.\n");
+    if (file_list) {
+        FILE *fp;
 
-        tinyfile_async(&arg, TINYFILE_COMPRESS, priority, &entry_index);
+        if ((fp = fopen(file_list_path, "r")) == NULL) {
+            fprintf(stderr, "ERROR: fopen() failed\n");
+            exit(EXIT_FAILURE);
+        }
 
-        printf("Asynchronous call tinyfile_async() made.\n");
-        printf("Now calling tinyfile_async_wait().\n");
+        int lines = 0, ch;
+        do {
+            if ((ch = fgetc(fp)) == '\n')
+                ++lines;
+        } while (ch != EOF);
 
-        /* Wait till computation done. */
-        tinyfile_async_wait(entry_index, &out);
+        if (fseek(fp, 0L, SEEK_SET) != 0) {
+            fprintf(stderr, "ERROR: fseek() failed\n");
+            exit(EXIT_FAILURE);
+        }
 
-        printf("tinyfile_async_wait() finished.\n");
-        printf("Compressed file = %s.\n", out.compressed_file_path);
+        tinyfile_arg_t args[lines], out[lines];
+        tinyfile_request_entry_idx_t keys[lines];
 
-        tinyfile_exit();
-    } else if (call_method == 's') {
-        /* Synchronous call to library */
-        printf("Making synchronous call to tinyfile_sync().\n");
+        char *line = NULL;
+        size_t len = 0, i = 0;
+        while (getline(&line, &len, fp) != -1) {
+            line[strcspn(line, "\n")] = 0; // Remove trailing newline
+            strcpy(args[i].source_file_path, line);
+            ++i;
+        }
 
-        tinyfile_sync(&arg, TINYFILE_COMPRESS, priority, &out);
+        fclose(fp);
+        free(line);
 
-        printf("Synchronous call completed.\n");
-        printf("Compressed file = %s.\n", out.compressed_file_path);
+        int j;
+        for (j = 0; j < lines; ++j) {
+            if (call_method == 'a') {
+
+                printf("Making non-blocking async call: tinyfile_async() for file %s.\n", args[j].source_file_path);
+                tinyfile_async(&args[j], TINYFILE_COMPRESS, priority, &keys[j]);
+
+            } else if (call_method == 's') {
+
+                printf("Making blocking sync call: tinyfile_sync() for file %s.\n", args[j].source_file_path);
+                tinyfile_sync(&args[j], TINYFILE_COMPRESS, priority, &out[j]);
+                printf("Sync call done. Compressed file: %s.\n", out[j].compressed_file_path);
+
+            } else {
+
+                fprintf(stderr, "ERROR: Unexpected state\n");
+                exit(EXIT_FAILURE);
+
+            }
+        }
+
+        if (call_method == 'a') {
+
+            printf("Calling tinyfile_async_join().\n");
+            tinyfile_async_join(keys, lines, out);
+            printf("tinyfile_async_join() completed. Compressed files:\n");
+
+            for (j = 0; j < lines; ++j)
+                printf("%s\n", out[j].compressed_file_path);
+
+        }
+    } else {
+        tinyfile_arg_t arg, out;
+        tinyfile_request_entry_idx_t key;
+
+        strcpy(arg.source_file_path, file_path);
+
+        if (call_method == 'a') {
+
+            printf("Making non-blocking async call: tinyfile_async() for file %s.\n", arg.source_file_path);
+            tinyfile_async(&arg, TINYFILE_COMPRESS, priority, &key);
+
+            printf("Async call done. Calling tinyfile_async_wait().\n");
+
+            tinyfile_async_wait(key, &out);
+            printf("tinyfile_async_wait() completed. Compressed file: %s.\n", out.compressed_file_path);
+
+        } else if (call_method == 's') {
+
+            printf("Making blocking sync call: tinyfile_sync() for file %s.\n", arg.source_file_path);
+            tinyfile_sync(&arg, TINYFILE_COMPRESS, priority, &out);
+            printf("Sync call done. Compressed file: %s.\n", out.compressed_file_path);
+
+        }
     }
 
     tinyfile_exit();
